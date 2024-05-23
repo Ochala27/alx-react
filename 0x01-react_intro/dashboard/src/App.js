c95ff0a730feddd89a46ed34cb9c24d90a6f912e
Ochala27 <ochalaonuche3@gmail.com>
2024-05-23 08:15:15 +0100

    Basic Application

--- 0x01-react_intro/dashboard/src/App.js
+++ 0x01-react_intro/dashboard/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
 import holbertonLogo from './holberton_logo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={holbertonLogo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Dashboard
-        </p>
+        <img src={holbertonLogo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </footer>
     </div>
   );
 }
