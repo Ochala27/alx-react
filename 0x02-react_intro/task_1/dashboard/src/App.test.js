ca52a6df24a1ae33df647e3a4ae09f6ed02cf2f6
Ochala27 <ochalaonuche3@gmail.com>
2024-05-26 18:45:32 +0100

    Write the tests for each component

--- 0x02-react_intro/task_1/dashboard/src/App.test.js
+++ 0x02-react_intro/task_1/dashboard/src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
+import Notifications from './components/Notifications/Notifications';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import Footer from './components/Footer/Footer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('contains the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications).exists()).toBe(true);
+  });
+
+  it('contains the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header).exists()).toBe(true);
+  });
+
+  it('contains the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login).exists()).toBe(true);
+  });
+
+  it('contains the Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer).exists()).toBe(true);
+  });
 });
+
