2e458f3a38c62b30258e0b903a93f2c4c5f003aa
Ochala27 <ochalaonuche3@gmail.com>
2024-06-19 18:25:15 +0100

    Bound the actions

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,36 @@
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+} from './uiActionTypes';
+import { bindActionCreators } from 'redux';
+
+export const login = (email, password) => ({
+  type: LOGIN,
+  user: { email, password },
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER,
+});
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER,
+});
+
+export const boundUIActionCreators = (dispatch) =>
+  bindActionCreators(
+    {
+      login,
+      logout,
+      displayNotificationDrawer,
+      hideNotificationDrawer,
+    },
+    dispatch
+  );
+
