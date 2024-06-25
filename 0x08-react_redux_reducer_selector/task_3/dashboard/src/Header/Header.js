95926023e8f01fdf5daf8712288b62a3cee9b533
Ochala27 <ochalaonuche3@gmail.com>
2024-06-25 18:31:09 +0100

    Create the reducer for notifications

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/Header/Header.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import logo from '../assets/logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+export default function Header(props) {
+  return (
+    <header className={css(styles.appHeader)}>
+      <img src={logo} className={css(styles.appLogo)} alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
+
+const styles = StyleSheet.create({
+  appHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: '#E11D3F',
+    borderBottom: '3px solid #E11D3F',
+  },
+  appLogo: {
+    height: '200px',
+    width: '200px',
+  },
+});
+
